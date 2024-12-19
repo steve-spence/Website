a8bb9a47fa2c8f161312e5418e5fa45e45c7d153
Steven Allen Spencer <stevenistotallyawesome@gmail.com>
2024-12-19 14:22:04 -0500

    updated some colors and updated the z indexes

--- js/Header.js
+++ js/Header.js
@@ -2,10 +2,12 @@
   const header = document.querySelector('header');
   let lastScrollY = window.scrollY;
 
+  // This executes 5 times in practice
   window.addEventListener('scroll', () => {
     if (window.scrollY < lastScrollY) {
       // Scrolling up - show header with falling effect
       header.classList.add('show');
+      console.log("added show");
     } else {
       // Scrolling down - hide header
       header.classList.remove('show');
