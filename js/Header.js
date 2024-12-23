7f12f0ea1beb99db2f892db97587e4a2bff4e964
Steven Allen Spencer <stevenistotallyawesome@gmail.com>
2024-12-22 23:33:17 -0500

    fixed the header not working on scrolling
    about to update some colors

--- js/Header.js
+++ js/Header.js
@@ -1,13 +1,13 @@
-
   const header = document.querySelector('header');
   let lastScrollY = window.scrollY;
 
   // This executes 5 times in practice
   window.addEventListener('scroll', () => {
+    console.log(window.scrollY);
     if (window.scrollY < lastScrollY) {
       // Scrolling up - show header with falling effect
       header.classList.add('show');
-      console.log("added show");
+      //console.log("added show");
     } else {
       // Scrolling down - hide header
       header.classList.remove('show');
